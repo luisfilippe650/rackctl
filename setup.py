cc781c6d52abc6b117b293eb5100bf896d6b8162
luisfilippe650 <lippe650.filippe@gmail.com>
2026-05-05 07:55:52 -0300

    feat: add dir debian

--- setup.py
+++ setup.py
@@ -4,15 +4,19 @@ setup(
     name="rackctl",
     version="1.0.0",
     description="CLI for communicating with the RackTables API",
+    author="Luis Filippe Reis Nogueira",
+    author_email="luis.nogueira@inpe.br",
     packages=find_packages(),
+    package_dir={"": "."},
     install_requires=[
         "requests",
         "python-dotenv",
-        "pyyaml"
+        "pyyaml",
     ],
     entry_points={
         "console_scripts": [
             "rackctl=src.__main__:main"
         ]
     },
+    python_requires=">=3.8",
 )
