72b021bc8088370ebaccf85b38ec3beeba4a9500
luisfilippe650 <lippe650.filippe@gmail.com>
2026-05-08 08:34:06 -0300

    (output) upgrade in output

--- src/utils/output.py
+++ src/utils/output.py
@@ -1,18 +1,29 @@
 import json
 
+
 def pretty_json(data):
     return json.dumps(data, indent=4, ensure_ascii=False)
 
+
 def print_response(response):
+
     ok = response.status_code < 400
-    symbol = "[+]" if ok else "[-]"
+
     method = response.request.method
     url = response.url
 
-    print(f"\n{symbol} {response.status_code} | {method} {url}")
-
     try:
-        print(pretty_json(response.json()))
+        response_content = response.json()
+
     except ValueError:
-        print(response.text)
+        response_content = response.text
+
+    output = {
+        "status_code": response.status_code,
+        "url": url,
+        "method": method,
+        "response": response_content
+    }
+
+    print(pretty_json(output))
     print()
